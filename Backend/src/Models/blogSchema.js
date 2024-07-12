2270a981c33d0d72030bfd0118024a2a6e6890af
Shivamkr025 <shivam22@navgurukul.org>
2024-07-12 11:09:29 +0530

    create user model for login and signup

--- Backend/src/Models/blogSchema.js
+++ Backend/src/Models/blogSchema.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const blogSchema = new mongoose.Schema({
 
     title: { type: String, require: true },
     body: { type: String, require: true },
-    author: { type: String, require: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', require: true },
     date: { type: Date, default: Date.now }
 })
 
-export const blog = mongoose.model('blog' , blogSchema)
+export const blog = mongoose.model('blog', blogSchema)
