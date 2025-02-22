6b49502047baea70a754ac1f4a9a431f32a95ea2
Shivamkr025 <shivam22@navgurukul.org>
2025-02-22 10:18:04 +0530

    update blog application

--- Backend/src/config/cloudinary.js
+++ Backend/src/config/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Add in your .env file
-    api_key: process.env.CLOUDINARY_API_KEY,       // Add in your .env file
-    api_secret: process.env.CLOUDINARY_API_SECRET  // Add in your .env file
+    cloud_name: "dzzz9qoia", // Add in your .env file
+    api_key: "732814373897312",       // Add in your .env file
+    api_secret: "2QYyym546eYi91sRKuEVUwS_VIw"  // Add in your .env file
 });
 
 export default cloudinary;
